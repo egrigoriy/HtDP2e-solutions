1a95707085bc2e05671c3b2acc4108cc4d18dd17
Grigoriy <egrigoriy@gmail.com>
2016-10-27 15:24:08 +0300

    exercise3 description added

--- FixedSize_Data/exercise3.rkt
+++ FixedSize_Data/exercise3.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;create an expression using string primitives that adds "_" at position i.
+;In general this means the resulting string is longer than the original one; here the expected result is "hello_world".
 (define str "helloworld") 
 (define i 5)
 (string-append (substring str 0 i) "_" (substring str i))
