158a31f35db0ae44c643817180ded5823c570f95
Grigoriy <egrigoriy@gmail.com>
2016-10-27 15:22:21 +0300

    exercise2 description added

--- FixedSize_Data/exercise2.rkt
+++ FixedSize_Data/exercise2.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;use string primitives to create an expression that concatenates prefix and suffix and adds "_" between them.
+;When you run this program, you will see "hello_world" in the interactions area.
 (define prefix "hello")
 (define suffix "world")
 (string-append prefix "_" suffix)
