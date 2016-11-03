ce24d090820933b8d1deaf509911f22166b227c3
Grigoriy <egrigoriy@gmail.com>
2016-11-03 07:29:47 +0300

    exercise12 solution added

---
+++ FixedSize_Data/exercise12.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise12) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;Exercise 12. Define the function cvolume, which accepts the length of a side of an equilateral cube and computes its volume.
+;If you have time, consider defining csurface, too
+
+(define (cvolume a) (* a a a))
+
+(cvolume 5)
+
+(define (csurface a) (* a a 6))
+
+(csurface 5)
